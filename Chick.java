f849754fd0e7254e10714ef4f98952143f6c3684
aliciazz <zengalicia257@gmail.com>
2015-10-26 08:38:39 -0700

    omd

--- Chick.java
+++ Chick.java
@@ -1,4 +1,25 @@
 class Chick implements Animal 
 {     
-     //your code here
+     private String myType;
+     private String mySound;
+     public Chick(String type, String sound) {
+       myType = type;
+       mySound = sound;
+     }
+     public Chick() {
+       myType = "unknown";
+       mySound = "unknown";
+     }
+     public void setSound(String s) {
+       mySound = s;
+     }
+     public String getSound() {
+       if (Math.random()> .5)
+       return mySound;
+       else
+       return "Cluck";
+     }
+     public String getType() {
+       return myType;
+     }
 }
--- Farm.java
+++ Farm.java
@@ -1,4 +1,15 @@
 class Farm 
 {     
-   //your code here
+   private Animal[] bAnimals = new Animal[3];
+   public Farm() {
+     bAnimals[0] = new NamedCow("Cow", " Susan", "Moo");
+     bAnimals[1] = new Chick("Chick", "Cheep");
+     bAnimals[2] = new Pig("Pig", "Oink");
+   }
+   public void animalSounds() {
+     for (int nI = 0; nI <bAnimals.length; nI++) {
+       System.out.println(bAnimals[nI].getType() + " goes " + bAnimals[nI].getSound());
+       System.out.println("The cow is called" + ((NamedCow)bAnimals[0]).getName());
+     }
+   }
 }
